2ac9cd4d258b46f69879bc3b0fa2b96578513047
Omry Maor <omry8880@gmail.com>
2024-01-13 20:31:50 +0200

    New mac who dis?

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,71 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
-    }
-    
-    public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
-    }
-
-    public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int[] input = { 1, 2, 3, 4, 5, 6 };
+        int[] input2 = { 1, 2, 4, 3, 5, 6, 2 };
+        // System.out.println("Missing int: " + findMissingInt(input));
+        // System.out.println("Second max value: " + secondMaxValue(input));
+        System.out.println("Contains the same elements: " + containsTheSameElements(input, input2));
     }
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+    public static int findMissingInt(int[] array) {
+        int missingNum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != i + 1)
+                missingNum = i + 1;
+        }
+        return missingNum;
     }
 
-    public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+    public static int secondMaxValue(int[] array) {
+        int max = 0;
+        int secondMax = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] != max && array[i + 1] != max) {
+                secondMax = Math.max(array[i], array[i + 1]);
+            }
+        }
+
+        return secondMax;
+    }
+
+    public static boolean containsTheSameElements(int[] array1, int[] array2) {
+        int[] maxArray, minArray;
+        if (array1.length != array2.length) {
+            maxArray = Math.max(array1.length, array2.length) == array1.length ? array1 : array2;
+            minArray = Math.min(array1.length, array2.length) == array1.length ? array1 : array2;
+        } else {
+            maxArray = array1;
+            minArray = array2;
+        }
+
+        for (int i = 0; i < maxArray.length; i++) {
+            for (int j = 0; j < minArray.length; j++) {
+                if (maxArray[i] == minArray[j]) {
+                    break;
+                } else if (j == minArray.length - 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSorted(int[] array) {
+        boolean decreases = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < array[i + 1]) {
+                decreases = true;
+                break;
+            }
+        }
+        return true;
     }
 
 }
